0686c182c3c30d7c27fef584879033a4a02bcc75
charlie  lee <ctlee9096@gmail.com>
2019-01-01 09:38:54 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,33 +4,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 //
 // let index = 0;
 // function onKeyDownHandler(e) {//https://stackoverflow.com/questions/31626852/how-to-add-konami-code-in-a-website-based-on-html
-//   //get value of key code from key map
-//   const key = parseInt(e.keycode||e.which)
-//   //compare e.which with code
-//   if(key === code[index]){
-//     //move to the next key in the code sequence
-//     index++;
-//     //if the last key is reached,
-//     if (index === code.length){
-//       //alert congratulatory
-//       alert('you found the code');
-//       index = 0;
-//     }
-//   } else {
-//     index = 0
-//   }
-// }
-// document.addEventListener('keydown', onKeyDownHandler);
-//   // Write your JavaScript code inside the init() function
-// }
-//events parameters: https://www.w3schools.com/jsref/dom_obj_event.asp
-//check for 'keydown' events. If the user enters this special code,
-//pressing all ten of the keys in the correct order,
-//alert() a congratulatory message. However,
-//if they press a key out of sequence or a key that isn't part of the Konami code,
-//don't alert() anything and
-//simply keep listening for all ten keydowns in the correct order.
-
 //When you're testing the code out in the browser,
 //be sure to call init() to attach the event listener and set everything up!
 function init() {
