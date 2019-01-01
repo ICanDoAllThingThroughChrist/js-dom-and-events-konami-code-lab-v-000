7e0b16ce9fe0e6d35f8ca78f75eaa3bebd4b1c07
charlie  lee <ctlee9096@gmail.com>
2019-01-01 07:59:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//You'll want to attach an event listener to document.body and 
+//check for 'keydown' events. If the user enters this special code,
+//pressing all ten of the keys in the correct order,
+//alert() a congratulatory message. However,
+//if they press a key out of sequence or a key that isn't part of the Konami code,
+//don't alert() anything and
+//simply keep listening for all ten keydowns in the correct order.
+
+//When you're testing the code out in the browser,
+//be sure to call init() to attach the event listener and set everything up!
+
 
 function init() {
   // Write your JavaScript code inside the init() function
